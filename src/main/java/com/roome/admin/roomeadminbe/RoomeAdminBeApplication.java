650ed1bebb797debef1e365fa0da011687f3e422
heyyeon <clsgks92@naver.com>
2025-09-10 00:56:49 +0900

    :sparkles: feat: 자동삭제(스케줄러)추가 구현

--- src/main/java/com/roome/admin/roomeadminbe/RoomeAdminBeApplication.java
+++ src/main/java/com/roome/admin/roomeadminbe/RoomeAdminBeApplication.java
@@ -2,7 +2,9 @@ package com.roome.admin.roomeadminbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class RoomeAdminBeApplication {
 
