7335fe6f52bf529dee99cb0585f4df90a942039a
heyyeon <clsgks92@naver.com>
2025-09-10 00:47:19 +0900

    :art: add: enum 추가 및 수정

--- src/main/java/com/roome/admin/roomeadminbe/domain/notification/type/NotificationCategory.java
+++ src/main/java/com/roome/admin/roomeadminbe/domain/notification/type/NotificationCategory.java
@@ -6,8 +6,10 @@ import lombok.Getter;
 public enum NotificationCategory {
 
     EVENT("이벤트 관리"),
-    SYSTEM("운영시스템 / 배포현황"),
-    NOTICE("공지사항");
+    SYSTEM("운영시스템 관련"),
+    CICD("배포/자동화 관련"),
+    USER("사용자 활동 관련"),
+    ETC("기타");
 
     private final String displayName;
 
