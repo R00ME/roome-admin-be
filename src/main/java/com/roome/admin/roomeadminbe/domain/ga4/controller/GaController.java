d424773d94d73a1a1c69da1e8eb5be12a67fa275
eunah0507 <eunah0507@naver.com>
2025-09-11 06:13:27 +0900

    ✨ 대시보드 api 기능 구현 완료

---
+++ src/main/java/com/roome/admin/roomeadminbe/domain/ga4/controller/GaController.java
@@ -0,0 +1,40 @@
+package com.roome.admin.roomeadminbe.domain.ga4.controller;
+
+import com.roome.admin.roomeadminbe.domain.ga4.dto.response.AiSummaryResponse;
+import com.roome.admin.roomeadminbe.domain.ga4.dto.response.ChartResponse;
+import com.roome.admin.roomeadminbe.domain.ga4.dto.response.SummaryResponse;
+import com.roome.admin.roomeadminbe.domain.ga4.service.GaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin/dashboard")
+@RequiredArgsConstructor
+public class GaController {
+    private final GaService gaService;
+
+    @GetMapping("/summary")
+    public ResponseEntity<List<SummaryResponse>> summary() {
+        List<SummaryResponse> list = gaService.getSumaary();
+        return ResponseEntity.status(HttpStatus.OK).body(list);
+    }
+
+    @GetMapping("/chart")
+    public ResponseEntity<List<ChartResponse>> chart(@RequestParam String typeId) {
+        List<ChartResponse> list = gaService.getChart(typeId);
+        return ResponseEntity.status(HttpStatus.OK).body(list);
+    }
+
+    @GetMapping("/ai-summary")
+    public ResponseEntity<AiSummaryResponse> aiSummary() {
+        AiSummaryResponse aiSummaryResponse = gaService.getAiSummary();
+        return ResponseEntity.status(HttpStatus.OK).body(aiSummaryResponse);
+    }
+}
