08c72d2ace8d4d1f3fe0b8a7031f3bcde6337bb3
9noeyni9 <koo949494@naver.com>
2025-09-14 02:15:37 +0900

    :recycle: fix: adminName, createdAt 조건으로 정렬 가능하도록 수정

--- src/main/java/com/roome/admin/roomeadminbe/domain/admin/repository/AdminRepositoryImpl.java
+++ src/main/java/com/roome/admin/roomeadminbe/domain/admin/repository/AdminRepositoryImpl.java
@@ -1,16 +1,21 @@
 package com.roome.admin.roomeadminbe.domain.admin.repository;
 
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.roome.admin.roomeadminbe.domain.admin.dto.request.AdminListRequest;
 import com.roome.admin.roomeadminbe.domain.admin.dto.response.AdminResponse;
 import com.roome.admin.roomeadminbe.domain.admin.entity.ActivationStatus;
+import com.roome.admin.roomeadminbe.domain.admin.entity.Admin;
 import com.roome.admin.roomeadminbe.domain.admin.entity.AdminRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -24,6 +29,8 @@ public class AdminRepositoryImpl implements AdminRepositoryCustom {
 
     @Override
     public Page<AdminResponse> findAll(AdminListRequest adminListRequest, Pageable pageable) {
+        OrderSpecifier<?> orderSpecifier = getOrderSpecifier(pageable);
+
         List<AdminResponse> list = jpaQueryFactory
                 .select(Projections.constructor(AdminResponse.class,
                         admin.adminId,
@@ -33,7 +40,7 @@ public class AdminRepositoryImpl implements AdminRepositoryCustom {
                 .from(admin)
                 .where(admin.activationStatus.eq(ActivationStatus.ACTIVE))
                 .where(adminRoleEq(adminListRequest.getRole()))
-                .orderBy(admin.createdAt.desc())
+                .orderBy(orderSpecifier)
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
@@ -48,5 +55,22 @@ public class AdminRepositoryImpl implements AdminRepositoryCustom {
 
     private BooleanExpression adminRoleEq(AdminRole adminRole) {
         return isEmpty(adminRole) ? null : admin.adminRole.eq(adminRole);
+    }
+
+    private OrderSpecifier<?> getOrderSpecifier(Pageable pageable) {
+        if (pageable.getSort().isEmpty()) {
+            return admin.createdAt.desc(); // 기본값
+        }
+
+        Sort.Order sortOrder = pageable.getSort().iterator().next();
+        Order direction = sortOrder.isAscending() ? Order.ASC : Order.DESC;
+        PathBuilder<?> entityPath = new PathBuilder<>(Admin.class, "admin");
+
+        return switch (sortOrder.getProperty()) {
+            case "adminName" -> new OrderSpecifier<>(direction, admin.adminName);
+            case "createdAt" -> // 가입순
+                    new OrderSpecifier<>(direction, admin.createdAt);
+            default -> new OrderSpecifier<>(Order.DESC, admin.createdAt);
+        };
     }
 }
