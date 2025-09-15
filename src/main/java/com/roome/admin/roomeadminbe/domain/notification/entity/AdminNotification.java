7568756aa161e62ddb046464e3913dda7cee6d59
heyyeon <clsgks92@naver.com>
2025-09-15 17:54:44 +0900

    :recycles :fix: 단¼ 읽음ì 여부 빌더 추가

--- src/main/java/com/roome/admin/roomeadminbe/domain/notification/entity/AdminNotification.java
+++ src/main/java/com/roome/admin/roomeadminbe/domain/notification/entity/AdminNotification.java
@@ -30,11 +30,11 @@ public class AdminNotification {
     @JoinColumn(name = "notification_id", nullable = false)
     private Notification notification;
 
-    @Column(name = "is_read", nullable = false)
-    private boolean isRead = false;
-
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @Builder.Default
+    private boolean isRead = false;
 
     public void markAsRead() {
         this.isRead = true;
